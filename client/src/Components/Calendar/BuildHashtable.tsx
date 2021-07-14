6dce16045eb95bf023764324b552b71637a906ee
wu-wilson <wilson.cui.wu@gmail.com>
2021-07-14 18:11:04 -0400

    cleaned code

--- client/src/Components/Calendar/BuildHashtable.tsx
+++ client/src/Components/Calendar/BuildHashtable.tsx
@@ -10,9 +10,9 @@ export type counts = {
 const buildHashtable = async (user_id: string) => {
   const table: { [key: string]: counts } = {};
   // Store the count of items in each day in data
-  const { data }: { data: counts[] } = await axios
-    .get(`${process.env.REACT_APP_API_URL}/count/${user_id}`)
-    .then();
+  const { data }: { data: counts[] } = await axios.get(
+    `${process.env.REACT_APP_API_URL}/count/${user_id}`
+  );
   // Put the counts into table
   data.forEach((elem) => (table[elem.day_id] = elem));
   return table;
